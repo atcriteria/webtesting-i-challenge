a5ecd7ad3c81856d5ed2ab9faa5f1c4ece69a39e
dude lao <brycelipscomb@gmail.com>
2021-01-27 20:51:19 -0500

    completed MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,44 @@
-module.exports = {
-  success,
-  fail,
-  repair,
-  get,
-};
+class Item {
+  constructor(name){
+    this.name = name;
+    this.durability = 100;
+    this.enhancement = 0;
+  }
+}
 
 function success(item) {
-  return { ...item };
+  if (item.enhancement < 20){
+    return { ...item,
+    ["enhancement"]: item.enhancement += 1 };
+  } else {
+    return { ...item }
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15){
+    return {...item, ["durability"]: item.durability -= 5}
+  } else {
+    if (item.enhancement > 16) {
+      return {...item, ["durability"]: item.durability -= 10, ["enhancement"]: item.enhancement -= 1}
+    } 
+  return {...item, ["durability"]: item.durability -=10}
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item,
+  ["durability"]: 100 };
 }
 
 function get(item) {
   return { ...item };
 }
+
+module.exports = {
+  success,
+  fail,
+  repair,
+  get,
+  Item
+};
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,49 @@
-const enhancer = require('./enhancer.js');
 // test away!
+const Item = require('./enhancer');
+
+let sword = new Item.Item('longsword');
+
+test('sanity', () => {
+    const testname = 'longsword'
+    const sword = new Item.Item(testname);
+    expect(sword.name).toBe('longsword')
+});
+
+it('can be repaired properly', () => {
+    sword.durability = 10;
+    expect(sword.durability).toBe(10);
+    sword = Item.repair(sword);
+    expect(sword.durability).toBe(100);
+});
+
+it('can be enhanced successfully', () => {
+    expect(sword.enhancement).toBe(0);
+    Item.success(sword);
+    expect(sword.enhancement).toBe(1);
+});
+
+describe('failure tests', () => {
+    let enhancedSword;
+    beforeEach(() => {
+        enhancedSword = new Item.Item('broadsword');
+    })
+    it('fails successfully less than 10enh', () => {
+        enhancedSword.enhancement = 10;
+        Item.fail(enhancedSword);
+        expect(enhancedSword.durability).toBe(95)
+    });
+
+    it('fails successfully @ 16enh', () => {
+        enhancedSword.enhancement = 16;
+        Item.fail(enhancedSword);
+        expect(enhancedSword.durability).toBe(90);
+        expect(enhancedSword.enhancement).toBe(16);
+    });
+
+    it('fails successfully above 16', () => {
+        enhancedSword.enhancement = 18;
+        Item.fail(enhancedSword);
+        expect(enhancedSword.durability).toBe(90);
+        expect(enhancedSword.enhancement).toBe(17);
+    })
+})
